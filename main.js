c5f9b2164fd5ce4f38a404257f6c284f50c59e93
e-lab innovations <mohammedashad23@gmail.com>
2019-11-24 20:52:31 +0530

    fetching from gdrive

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const gallery = document.querySelector('#gallery');
 
+/*
 function loadJSON(callback) {   
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
@@ -24,3 +25,45 @@ function loadJSON(callback) {
 });
 
 //gallery.innerHTML = 'Hello'
+
+*/
+
+const url = "https://script.google.com/macros/s/AKfycbxbq55KNvvg64TZRjfQOqLEp-q-5iyZL53PuedR29Jkdimjk0vH/exec"
+
+
+// Get data from json api
+ function getData(e){
+ 	const xhr = new XMLHttpRequest();
+ 	console.log("Clicked");
+
+ 	xhr.open("GET", url, true);
+
+ 	xhr.onload = function(){
+ 		if(this.status === 200){
+ 			const res = JSON.parse(this.responseText);
+
+ 			let output = "";
+
+ 			res.forEach((item) => {
+ 				output += `
+ 							<div class="col-lg-3 col-md-4 col-xs-6">
+ 							<a href="${item.url}" class="d-block mb-4 h-100" target="_blank">
+ 							<img class="img-fluid img-thumbnail" src="${item.url}" alt="${item.name}">
+ 							</a>
+ 							</div>
+ 				 		`
+ 			});
+
+ 			gallery.innerHTML = output;
+ 		}
+ 		else{
+ 			gallery.innerHTML = `<li>Something went wrong</li>`
+ 		}
+ 	}
+
+ 	xhr.send();
+
+ 	e.preventDefault();
+ }
+ 
+ getData()
